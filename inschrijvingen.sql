5254bda32627d67538f287689c3abd340cd14c2d
antmaster2001 <46071322+antmaster2001@users.noreply.github.com>
2019-05-08 09:56:22 +0200

    Opdracht 2

---
+++ inschrijvingen.sql
@@ -0,0 +1,12 @@
+USE `lo8e_sql2`;
+
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES  ("lo9e-amo1", "vlt03", 2019, "amo"),
+		("lo9e-amo2", "rs001", 2019, "amo"),
+        ("lo9e-amo3", "bkr02", 2019, "amo");
+        
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES  (90000,'Sixx','','Nikki','1962-07-27','2200 SG','Los Angeles', 75, 1.86, 07-01-2015),
+		(90500,'Hudson','','Saul','1963-07-27','2203 SG','Hollywood', 70, 1.75, 05-01-2011),
+        (90050,'Richards','','Keith','1954-07-27','2209 SG','London', 67.2, 1.56, 05-01-2013),
+        (95000,'Vaughan','Ray','Stevie','1971-07-27','2218 SG','Texas', 78.2, 1.66, 04-01-2012);
