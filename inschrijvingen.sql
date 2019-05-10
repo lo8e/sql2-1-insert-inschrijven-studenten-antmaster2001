10485d38347ced74da05b49347e3c8eab3ba3617
antmaster2001 <46071322+antmaster2001@users.noreply.github.com>
2019-05-10 12:23:41 +0200

    Opdracht6

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -25,3 +25,13 @@ WHERE ov_nummer = 90000;
 UPDATE studenten
 SET actief = false
 WHERE inschrijvings_datum <= "1-1-2017";
+
+SELECT concat(studenten.voornaam,' ', studenten.tussenvoegsel,' ', studenten.achternaam) AS Completepnaam, klassen.cohort , klassen.klas_code, klassen_studenten.ov_nummer, concat(docenten.voorletters, ' ', docenten.tussenvoegsel, ' ', docenten.achternaam) AS SLBdocent FROM studenten
+INNER JOIN klassen_studenten 
+ON klassen_studenten.ov_nummer = studenten.ov_nummer 
+INNER JOIN klassen
+ON klassen.klas_code = klassen_studenten.klas_code
+INNER JOIN docenten 
+ON klassen.slb_code = docenten.docent_code
+WHERE studenten.actief = true
+ORDER BY  klassen.klas_code ASC, studenten.achternaam ASC;
