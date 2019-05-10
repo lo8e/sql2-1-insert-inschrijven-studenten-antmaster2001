2a68d04b7972d03f59510a209553e9c4dd06ab7b
antmaster2001 <46071322+antmaster2001@users.noreply.github.com>
2019-05-10 11:47:16 +0200

    Opdracht3

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -10,3 +10,11 @@ VALUES  (90000,'Sixx','','Nikki','1962-07-27','2200 SG','Los Angeles', 75, 1.86,
 		(90500,'Hudson','','Saul','1963-07-27','2203 SG','Hollywood', 70, 1.75, 05-01-2011),
         (90050,'Richards','','Keith','1954-07-27','2209 SG','London', 67.2, 1.56, 05-01-2013),
         (95000,'Vaughan','Ray','Stevie','1971-07-27','2218 SG','Texas', 78.2, 1.66, 04-01-2012);
+
+INSERT INTO klassen_studenten(ov_nummer, klas_code) VALUES
+	(90000,'lo8e-amo1'),
+	(90500,'lo8e-amo2'),
+	(90050,'lo8e-amo3'),
+	(95000,'lo8e-amo4');
+    
+
